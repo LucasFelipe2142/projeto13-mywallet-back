7b21b1998921fd92184acd20ed07943be4ac1116
Lucas <lucasfelicio378@email.com>
2022-08-30 16:54:13 -0300

    receiving, validating and saving body

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
-import expres from 'express';
+import express from 'express';
 import cors from 'cors';
+import Joi from 'joi';
+
+const schema = Joi.object().keys({
+  name: Joi.string().min(1).required(),
+});
+
+const login = [
+  {
+    name: 'jorge',
+  },
+  {
+    name: 'lucas',
+  },
+  {
+    name: 'farlei',
+  },
+  {
+    name: 'carine',
+  },
+];
 
 const app = express();
-app.use(expres().json);
+app.use(express.json());
 app.use(cors());
 
+app.post('/participants', (req, res) => {
+  const result = schema.validate(req.body, Joi.messages);
+  const ok = true;
+  if (result.error) {
+    res.send(result.error.details);
+    ok = false;
+  } else {
+    for (let i = 0; i < login.length; i++) {
+      if (req.body.name === login[i].name) {
+        res.sendStatus(409);
+        ok = false;
+      }
+    }
+  }
+  if (ok) {
+    login.push({...req.body, lastStatus: Date.now()});
+    res.send(login);
+  }
+});
 
 app.listen(5000);
