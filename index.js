6b16d22c9b62435302f7cf598b32b515ddb01e26
Lucas <lucasfelicio378@email.com>
2022-08-30 21:38:29 -0300

    insertion get participants and post message but not in a mongodb

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const schema = Joi.object().keys({
   name: Joi.string().min(1).required(),
 });
 
+const schemaMessage = Joi.object().keys({
+  to: Joi.string().min(1).required(),
+  text: Joi.string().min(1).required(),
+  type: Joi.string().valid('message', 'private_message'),
+
+});
+
 const login = [];
 
 const messageAPI = [];
@@ -43,9 +50,43 @@ app.post('/participants', (req, res) => {
       type: 'status',
       time: hora + ':' + min + ':' + seg,
     });
-
     login.push({...req.body, lastStatus: Date.now()});
-    res.send(messageAPI);
+    res.sendStatus(201);
+  }
+});
+
+app.get('/participants', (req, res) => {
+  res.send(login);
+});
+
+app.get('/messages', (req, res) => {
+  const data = new Date();
+  const hora = data.getHours();
+  const min = data.getMinutes();
+  const seg = data.getSeconds();
+  let aux = false;
+
+  const result = schemaMessage.validate(req.body, Joi.messages);
+
+  for (let i = 0; i < login.length; i++) {
+    if (req.headers.user === login[i].name) {
+      aux = true;
+    }
+  }
+
+  if (result.error) {
+    res.sendStatus(422);
+  } else
+  if (!aux) res.sendStatus(422);
+  else {
+    messageAPI.push({
+      from: req.headers.user,
+      to: req.body.to,
+      text: req.body.text,
+      type: req.body.type,
+      time: hora + ':' + min + ':' + seg,
+    });
+    res.sendStatus(201);
   }
 });
 
