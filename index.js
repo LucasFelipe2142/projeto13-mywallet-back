a9bce2c339b537554afdaec149d7894ac0ba5915
Lucas <lucasfelicio378@email.com>
2022-09-08 12:39:37 -0300

    login, registration and add or remove value, complete

--- index.js
+++ index.js
@@ -1,146 +1,132 @@
-import express from 'express';
-import cors from 'cors';
-import Joi from 'joi';
-import { MongoClient } from 'mongodb';
+import express from "express";
+import cors from "cors";
+import Joi from "joi";
+import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import { v4 as uuid } from "uuid";
+import bcrypt from "bcrypt";
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.URI);
 
 let db;
 
-const app = newFunction();
+const app = start();
 
 mongoClient.connect().then(() => {
-	db = mongoClient.db('chatUol');
+  db = mongoClient.db("walletBD");
 });
 
 const schema = Joi.object().keys({
   name: Joi.string().min(1).required(),
 });
 
-const schemaMessage = Joi.object().keys({
-  to: Joi.string().min(1).required(),
-  text: Joi.string().min(1).required(),
-  type: Joi.string().valid('message', 'private_message'),
-
+const schemaRegistration = Joi.object().keys({
+  name: Joi.string().min(1).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  password: Joi.string().min(6).required(),
 });
 
-app.post('/participants', (req, res) => {
-  const data = new Date();
-  const hora = data.getHours();
-  const min = data.getMinutes();
-  const seg = data.getSeconds();
+const schemaAddorRemove = Joi.object().keys({
+  type: Joi.valid("add", "remove").required(),
+  description: Joi.string().min(1).required(),
+  valor: Joi.string().min(1).required(),
+});
 
-  const result = schema.validate(req.body, Joi.messages);
+app.post("/cadastro", async (req, res) => {
+  const result = schemaRegistration.validate(req.body, { abortEarly: true });
 
   if (result.error) {
-    
-    res.send(result.error.details);
-  }else{
-    db.collection("logarBD").findOne({
-    name: req.body.name
-  }).then(user => {
-     if(user !== null) res.sendStatus(409);
-     else{
-        db.collection("messageBD").insertOne({
-          from: req.body.name,
-          to: 'Todos',
-          text: 'entra na sala...',
-          type: 'status',
-          time: hora + ':' + min + ':' + seg,
-        });
-      
-      db.collection("logarBD").insertOne({...req.body, lastStatus: Date.now()});
-      res.sendStatus(201);
-     }
-     
-  });
-  }
-});
-
-app.get('/participants', (req, res) => {
-  db.collection("logarBD").find().toArray().then(user => res.send(user))
-});
-
-app.post('/messages', (req, res) => {
-  const data = new Date();
-  const hora = data.getHours();
-  const min = data.getMinutes();
-  const seg = data.getSeconds();
-
-  const result = schemaMessage.validate(req.body, Joi.messages);
-
-  if (result.error) {
-    res.sendStatus(422);
-  } else{ 
-      db.collection("logarBD").findOne({
-        name: req.headers.user
-      }).then(user => {
-        if(user === null) res.sendStatus(422);
-        else{
-          db.collection("messageBD").insertOne({
-            from: req.headers.user,
-            to: req.body.to,
-            text: req.body.text,
-            type: req.body.type,
-            time: hora + ':' + min + ':' + seg,
+    res.sendStatus(404);
+  } else {
+    db.collection("usersBD")
+      .findOne({
+        email: req.body.email,
+      })
+      .then((user) => {
+        if (user !== null) res.sendStatus(409);
+        else {
+          const password = bcrypt.hashSync(req.body.password, 10);
+          db.collection("usersBD").insertOne({
+            ...req.body,
+            password: password,
           });
           res.sendStatus(201);
         }
       });
-    }
+  }
 });
 
-app.get('/messages', (req, res) => {
-  const numMessages = req.query.limit === undefined ? 2 : req.query.limit;
-  db.collection("messageBD").find({$or:[{to: req.headers.user},{type: 'message'},{type: 'status'},{from: req.headers.user}]}).toArray().then(user => res.send(user.slice(-numMessages)));
+app.get("/cadastro", (req, res) => {
+  db.collection("usersBD")
+    .find()
+    .toArray()
+    .then((user) => {
+      res.send(user);
+    });
 });
 
-app.post('/status', (req, res) => {
-
-  db.collection("logarBD").findOne({
-    name: req.headers.user
-  }).then(user => {
-     if(user === null) res.sendStatus(404);
-     else{
-      db.collection("logarBD").update({name: req.headers.user},{$set:{lastStatus: Date.now()}})
-      res.sendStatus(200);
-     }
-     
-  });
-  
-});
-
-setInterval(()=>{
-  db.collection("logarBD").find().toArray().then(list => {
-     if(list !== null){
-      for(let i=0; i<list.length; i++){
-        if(Date.now() - list[i].lastStatus > 10000){
-          const data = new Date();
-          const hora = data.getHours();
-          const min = data.getMinutes();
-          const seg = data.getSeconds();
-          db.collection("messageBD").insertOne({
-            from: list[i].name,
-            to: 'Todos',
-            text: 'sai da sala...',
-            type: 'status',
-            time: hora + ':' + min + ':' + seg,
+app.post("/login", async (req, res) => {
+  db.collection("usersBD")
+    .findOne({
+      email: req.body.email,
+    })
+    .then((user) => {
+      if (user === null) return res.send(404);
+      else if (user && bcrypt.compareSync(req.body.password, user.password)) {
+        const token = uuid();
+        db.collection("sessionsBD")
+          .insertOne({
+            token: token,
+            userId: user._id,
+          })
+          .then(() => {
+            delete user.password;
+            res.send(token);
           });
-          db.collection("logarBD").remove({name:list[i].name});
-        }
+      } else {
+        res.send(404);
       }
-     }    
-  });
-}, 15000);
+    });
+});
+
+app.post("/add_or_remove_value", async (req, res) => {
+  const token = req.headers.authorization?.replace("Bearer ", "");
+  const result = schemaAddorRemove.validate(req.body, { abortEarly: true });
+
+  if (result.error) {
+    res.sendStatus(404);
+  } else {
+    db.collection("sessionsBD")
+      .findOne({
+        token: token,
+      })
+      .then((value) => {
+        if (value === null) return res.send(404);
+        else {
+          db.collection("valorsBD")
+            .insertOne({
+              ...req.body,
+              userId: value.userId,
+            })
+            .then((value) => {
+              res.send({
+                ...req.body,
+                userId: value.userId,
+              });
+            });
+        }
+      });
+  }
+});
 
 app.listen(5000);
 
-function newFunction() {
+function start() {
   const app = express();
   app.use(express.json());
   app.use(cors());
   return app;
 }
-
